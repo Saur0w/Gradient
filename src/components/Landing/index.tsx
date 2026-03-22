e4bf4b4d905364b5951c9e98d009674bcb546689
saur0w <sthapliyal085@gmail.com>
2026-03-22 06:57:34 +0530

    "Structure"

---
+++ src/components/Landing/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import styles from "./style.module.scss";
+
+export default function Landing() {
+    return (
+      <section className={styles.landing}>
+          <h1>Hi! I&#39;m Saurow, a designer specializing in Interactive Experiences</h1>
+      </section>  
+    );
+}
