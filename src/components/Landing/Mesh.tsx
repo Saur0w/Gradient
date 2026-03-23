74cf05b744edd9751cbb127f9b6a991c423169fe
saur0w <sthapliyal085@gmail.com>
2026-03-23 22:11:26 +0530

    "Uniforms"

--- src/components/Landing/Mesh.tsx
+++ src/components/Landing/Mesh.tsx
@@ -1,15 +1,28 @@
 "use client";
 
-import {} from "@react-three/fiber";
+import { useThree } from "@react-three/fiber";
+import { vertexShader, fragmentShader } from "@/lib/Shaders";
+import { useMemo } from "react";
 
 export default function Mesh() {
+    const { size } = useThree();
+    const uniforms = useMemo(() => ({
+        uResolution: { value: [size.width, size.height] },
+        uColorA: { value: [0.1, 0.0, 0.3] },
+        uColorB: { value: [1.0, 0.4, 0.0] },
+    }), [size]);
     return (
         <group>
             <mesh
                 position={[4, 0, 0]}
             >
                 <planeGeometry args={[2, 2]} />
-                <meshBasicMaterial color="purple"/>
+                <shaderMaterial
+                    vertexShader={vertexShader}
+                    fragmentShader={fragmentShader}
+                    uniforms={uniforms}
+                    depthWrite={false}
+                />
             </mesh>
         </group>
     )
