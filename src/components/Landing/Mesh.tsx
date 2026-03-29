820cef20fb882f7d62efbf83f0882e91074ec265
saur0w <sthapliyal085@gmail.com>
2026-03-29 11:09:08 +0530

    "Mesh"

--- src/components/Landing/Mesh.tsx
+++ src/components/Landing/Mesh.tsx
@@ -1,29 +1,42 @@
 "use client";
 
-import { useThree } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 import { vertexShader, fragmentShader } from "@/lib/Shaders";
+import * as THREE from "three";
 import { useMemo } from "react";
 
 export default function Mesh() {
     const { size } = useThree();
+
     const uniforms = useMemo(() => ({
-        uResolution: { value: [size.width, size.height] },
-        uColorA: { value: [1.0, 0.9, 0.88] },
-        uColorB: { value: [0.96, 0.6, 0.7] },
-    }), [size]);
+        uTime: { value: 0 },
+        uResolution: { value: new THREE.Vector2(size.width, size.height)},
+        uMouse: { value: new THREE.Vector2(0, 0)},
+        uColorA: { value: new THREE.Vector3(1.0, 0.9, 0.88) },
+        uColorB: { value: new THREE.Vector3(0.96, 0.6, 0.7) },
+    }), []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    useFrame(({ clock, size: s }) => {
+        //eslint-disable-next-line
+        uniforms.uTime.value = clock.getElapsedTime();
+        uniforms.uResolution.value.set(s.width, s.height);
+    });
+
+    const handleMouseMove = (e: THREE.Event) => {
+        const { clientX, clientY } = e as unknown as PointerEvent;
+        uniforms.uMouse.value.set(
+            (clientX / size.width)  *  2 - 1,
+            -(clientY / size.height) *  2 + 1,
+        );
+    };
+
     return (
-        <group>
-            <mesh
-                position={[4, 0, 0]}
-            >
-                <planeGeometry args={[2, 2]} />
-                <shaderMaterial
-                    vertexShader={vertexShader}
-                    fragmentShader={fragmentShader}
-                    uniforms={uniforms}
-                    depthWrite={false}
-                />
-            </mesh>
-        </group>
-    )
+        <mesh onPointerMove={handleMouseMove}>
+            <planeGeometry args={[2, 2]} />
+            <shaderMaterial vertexShader={vertexShader}
+                            fragmentShader={fragmentShader}
+                            uniforms={uniforms}
+                            depthWrite={false}/>
+        </mesh>
+    );
 }
